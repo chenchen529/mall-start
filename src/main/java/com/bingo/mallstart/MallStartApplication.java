68ddd14d872b9fe606a23ba689dd1c934dc1ae77
CHENZHANGBIN <chenzhangbin@kingsoft.com>
2019-05-27 17:56:11 +0800

    create a project

---
+++ src/main/java/com/bingo/mallstart/MallStartApplication.java
@@ -0,0 +1,13 @@
+package com.bingo.mallstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallStartApplication.class, args);
+	}
+
+}
