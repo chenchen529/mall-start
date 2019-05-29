507a7366d73706d3dd982f5939a8a2c611bd5cfb
CHENZHANGBIN <chenzhangbin@kingsoft.com>
2019-05-29 17:01:22 +0800

    添加品牌管理的接口

---
+++ src/main/java/com/bingo/mallstart/service/impl/PmsBrandServiceImpl.java
@@ -0,0 +1,47 @@
+package com.bingo.mallstart.service.impl;
+
+import com.bingo.mallstart.mapper.PmsBrandMapper;
+import com.bingo.mallstart.model.PmsBrand;
+import com.bingo.mallstart.model.PmsBrandExample;
+import com.bingo.mallstart.service.PmsBrandService;
+import com.github.pagehelper.PageHelper;
+
+import java.util.List;
+
+public class PmsBrandServiceImpl implements PmsBrandService {
+
+    private PmsBrandMapper brandMapper;
+
+    @Override
+    public List<PmsBrand> listAllBrand() {
+        return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public int createBrand(PmsBrand pmsBrand) {
+        return brandMapper.insertSelective(pmsBrand);
+    }
+
+    @Override
+    public int updateBrand(Long id, PmsBrand pmsBrand) {
+        pmsBrand.setId(id);
+        return brandMapper.updateByPrimaryKeySelective(pmsBrand);
+    }
+
+    @Override
+    public int deleteBrand(Long id) {
+        return brandMapper.deleteByPrimaryKey(id);
+    }
+
+    @Override
+    public List<PmsBrand> listBrand(Integer pageNum, Integer pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        brandMapper.selectByExample(new PmsBrandExample());
+        return brandMapper.selectByExample(new PmsBrandExample());
+    }
+
+    @Override
+    public PmsBrand getBrand(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+}
